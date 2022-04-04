f15c9d130980645cc32eb8f5925c85eb983f1119
Archie Torres <xtremesama@gmail.com>
2022-04-03 20:38:19 -0700

    Added react-dom and added a new homepage

---
+++ src/pages/HomePage.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const HomePage = () => (
+    <React.Fragment>
+        <h1>Hello, welcome to my blog!</h1>
+        <p>
+            Cras leo, condimentum erat. Consectetur iaculis leo viverra gravida dui. Condimentum aptent rhoncus ornare turpis. Felis natoque malesuada ut lacinia congue iaculis orci justo. Orci id sollicitudin potenti enim pharetra malesuada enim fusce lacus magnis! Sed justo semper proin dictum libero enim? Sem neque tortor neque parturient et magna donec interdum. Vel dolor nisi.
+        </p>
+        <p>
+            A sollicitudin accumsan dictum lacinia libero nisl egestas luctus faucibus ridiculus. Primis tellus cum placerat, ultrices eros leo nibh mauris etiam. Tempus natoque duis cursus sagittis etiam nisi. Vulputate conubia lacus volutpat neque ullamcorper pellentesque. Tempor ut class faucibus eros sodales netus tortor per nibh hac. Lacus ad habitasse nostra tempor torquent torquent euismod placerat vel dictum auctor vulputate. Fermentum molestie fringilla ultricies nam. Dignissim.
+        </p>
+        <p>
+            Sociis orci mus mollis integer sagittis. Iaculis cras consequat quisque. Diam ullamcorper facilisis semper auctor iaculis nascetur velit. Natoque venenatis aliquet natoque nullam rhoncus nascetur maecenas velit massa pulvinar pharetra. Porttitor, montes felis curabitur praesent habitasse condimentum iaculis adipiscing praesent. Fames lectus quisque vehicula facilisi at! Tristique praesent nisi donec. Arcu litora nec sociosqu.
+        </p>
+        <p>
+            Posuere sociis iaculis maecenas. Ornare ante, odio pretium potenti at tempus arcu lacus nostra vitae aenean. Risus a in arcu eget ut laoreet bibendum euismod mus sagittis quam dapibus. Lacinia turpis potenti porta sit odio nisi etiam. Rutrum parturient vel, eros adipiscing quisque dictum magna. Lobortis ultrices eleifend luctus lectus euismod.
+        </p>
+        <p>
+            Torquent conubia dignissim luctus congue. Sollicitudin, vitae accumsan lobortis sapien magnis mauris placerat. Luctus conubia taciti eu varius suscipit congue phasellus nascetur? Nec nunc porttitor porttitor nibh tempor accumsan metus nascetur vivamus leo pulvinar integer. Vestibulum faucibus at tellus orci? Gravida litora dui dolor fames odio vel. Vestibulum vehicula cursus vivamus. Nisl, eleifend cubilia enim ultricies dui velit mi. Consectetur praesent lectus dis eu aptent posuere dictumst ipsum nam tellus penatibus dignissim. Laoreet blandit senectus pulvinar. Accumsan est aliquam nec adipiscing risus vitae inceptos pharetra. Id ridiculus himenaeos lacinia ut est pharetra lacus id.
+        </p>
+    </React.Fragment>
+);
+
+export default HomePage;
