54b4fdb286d502415e67e0e9f424e90dabba6aea
Archie Torres <xtremesama@gmail.com>
2022-04-09 00:06:05 -0700

    Added comment form

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import ArticlesList from "../components/ArticlesList";
 import CommentsList from "../components/CommentsList";
 import UpvotesSection from "../components/UpvotesSection";
+import AddCommentForm from "../components/AddCommentForm";
 import articleContent from './article-content';
 import NotFoundPage from "./NotFoundPage";
 
@@ -40,6 +41,7 @@ const ArticlePage = ({ match }) => {
             ))}
 
             <CommentsList comments={articleInfo.comments} />
+            <AddCommentForm articleName={name} setArticleInfo={setArticleInfo} />
             <h3>Other Articles:</h3>
             <ArticlesList articles={otherArticles} />
         </React.Fragment>
